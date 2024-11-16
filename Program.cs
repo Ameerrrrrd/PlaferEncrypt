96db8a3eebdc6ca28e69edbda93fc91d8c183db2
hg gh <tutameer@gmail.com>
2024-11-16 15:19:39 +0300

    fix2

--- Program.cs
+++ Program.cs
@@ -4,20 +4,15 @@ using System.Linq;
 
 class PlayfairCipher
 {
-    // Статическая таблица для шифрования
     private static char[,] table = new char[5, 5];
 
-    // Словарь для быстрого поиска позиции буквы в таблице
     private static Dictionary<char, (int, int)> positionDict = new Dictionary<char, (int, int)>();
 
-    // Метод для генерации таблицы 5x5 из ключа
     private static void GenerateTable(string key)
     {
-        // Убираем все символы, кроме букв, и переводим в верхний регистр
         key = new string(key.Where(c => char.IsLetter(c)).Select(c => char.ToUpper(c)).ToArray());
 
-        // Объединяем ключ и алфавит (I и J считаем одинаковыми)
-        string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";  // Без J
+        string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";  
         string combinedKey = key + alphabet;
 
         // Убираем повторяющиеся символы
@@ -35,24 +30,21 @@ class PlayfairCipher
         }
     }
 
-    // Метод для разделения текста на пары букв
     private static List<string> PrepareText(string text)
     {
-        // Убираем все символы, кроме букв
         text = new string(text.Where(c => char.IsLetter(c)).Select(c => char.ToUpper(c)).ToArray());
 
         // Заменяем J на I
         text = text.Replace('J', 'I');
 
-        // Разделяем на пары
         List<string> pairs = new List<string>();
         for (int i = 0; i < text.Length; i += 2)
         {
             if (i + 1 < text.Length)
             {
-                if (text[i] == text[i + 1]) // Если одинаковые буквы
+                if (text[i] == text[i + 1])
                 {
-                    pairs.Add(text[i] + "X"); // Добавляем X как разделитель
+                    pairs.Add(text[i] + "X"); 
                     i--;
                 }
                 else
@@ -62,49 +54,47 @@ class PlayfairCipher
             }
             else
             {
-                pairs.Add(text[i] + "X"); // Если нечётное количество букв, добавляем X в конец
+                pairs.Add(text[i] + "X"); 
             }
         }
         return pairs;
     }
 
-    // Метод для шифрования пары букв
     private static string EncryptPair(string pair)
     {
         var (x1, y1) = positionDict[pair[0]];
         var (x2, y2) = positionDict[pair[1]];
 
-        if (x1 == x2) // Буквы в одной строке
+        if (x1 == x2) 
         {
             y1 = (y1 + 1) % 5;
             y2 = (y2 + 1) % 5;
         }
-        else if (y1 == y2) // Буквы в одном столбце
+        else if (y1 == y2) 
         {
             x1 = (x1 + 1) % 5;
             x2 = (x2 + 1) % 5;
         }
-        else // Буквы в разных строках и столбцах
+        else 
         {
-            (y1, y2) = (y2, y1); // Меняем столбцы
+            (y1, y2) = (y2, y1);
         }
 
         return $"{table[x1, y1]}{table[x2, y2]}";
     }
 
-    // Метод для шифрования текста
     public static string Encrypt(string text, string key)
     {
-        GenerateTable(key); // Генерация таблицы на основе ключа
-        var pairs = PrepareText(text); // Разбиение текста на пары
-        var encryptedText = string.Join("", pairs.Select(pair => EncryptPair(pair))); // Шифрование каждой пары
+        GenerateTable(key); 
+        var pairs = PrepareText(text); 
+        var encryptedText = string.Join("", pairs.Select(pair => EncryptPair(pair))); 
         return encryptedText;
     }
 
     static void Main(string[] args)
     {
-        string key = "Khan"; // Ваш ключ
-        string text = "I am Khan Ameer, and i'm autistic";  // Ваше сообщение
+        string key = "Khan";
+        string text = "I am Khan Ameer, and i'm autistic";  
 
         string encryptedText = Encrypt(text, key);
         Console.WriteLine("Зашифрованный текст: " + encryptedText);
